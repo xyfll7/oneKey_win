e7e6cc8f00343e8f076a7a1fae779e6646a9e7b9
小洋粉 <l.7@qq.com>
2022-09-17 21:28:47 +0800

    添加项目文件。

---
+++ oneKey_win/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using oneKey_win.utils;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace oneKey_win
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private TaskbarIcon? notifyIcon;
+        private Hotkey_init hotkey_Init = new Hotkey_init();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //create the notifyicon (it's a resource declared in NotifyIconResources.xaml
+            notifyIcon = (TaskbarIcon)FindResource("NotifyIcon");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            MessageBox.Show("推出");
+            notifyIcon?.Dispose(); //the icon would clean up automatically, but this is cleaner
+            base.OnExit(e);
+        }
+    }
+}
---
+++ oneKey_win/utils/Hotkey_init.cs
@@ -0,0 +1,86 @@
+﻿using GlobalHotKey;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using WindowsInput;
+
+namespace oneKey_win.utils
+{
+    internal class Hotkey_init
+    {
+
+        private HotKeyManager hotKeyManager_B = new HotKeyManager();
+        private HotKeyManager hotKeyManager_G = new HotKeyManager();
+        private HotKeyManager hotKeyManager_B_T = new HotKeyManager();
+        private HotKeyManager hotKeyManager_G_T = new HotKeyManager();
+        public Hotkey_init()
+        {
+            var hotKeyB = hotKeyManager_B.Register(Key.B, ModifierKeys.Control);
+            var hotKeyG = hotKeyManager_G.Register(Key.G, ModifierKeys.Control);
+            var hotKeyB_T = hotKeyManager_B_T.Register(Key.B, ModifierKeys.Control | ModifierKeys.Alt);
+            var hotKeyG_T = hotKeyManager_G_T.Register(Key.G, ModifierKeys.Control | ModifierKeys.Alt);
+            hotKeyManager_B.KeyPressed += HotKeyManagerPressed_B;
+            hotKeyManager_G.KeyPressed += HotKeyManagerPressed_G;
+            hotKeyManager_B_T.KeyPressed += HotKeyManagerPressed_B_T;
+            hotKeyManager_G_T.KeyPressed += HotKeyManagerPressed_G_T;
+        }
+
+
+        private void HotKeyManagerPressed_B(object? sender, KeyPressedEventArgs e)
+        {
+            if (e.HotKey.Key == Key.B)
+            {
+                var sim = new InputSimulator();
+                sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C);
+                string str = ClipboardGetText();
+                Process.Start(new ProcessStartInfo { FileName = $"https://www.baidu.com/s?wd={str}", UseShellExecute = true });
+            }
+        }
+        private void HotKeyManagerPressed_G(object? sender, KeyPressedEventArgs e)
+        {
+            if (e.HotKey.Key == Key.G)
+            {
+                var sim = new InputSimulator();
+                sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C);
+                string str = ClipboardGetText();
+                Process.Start(new ProcessStartInfo { FileName = $"https://www.google.com.hk/search?q={str}", UseShellExecute = true });
+            }
+        }
+
+        private void HotKeyManagerPressed_B_T(object? sender, KeyPressedEventArgs e)
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard.Sleep(1000).ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C).Sleep(50);
+            string str = ClipboardGetText();
+            Process.Start(new ProcessStartInfo { FileName = $"https://fanyi.baidu.com/#zh/en/{str}", UseShellExecute = true });
+        }
+        private void HotKeyManagerPressed_G_T(object? sender, KeyPressedEventArgs e)
+        {
+            var sim = new InputSimulator();
+            sim.Keyboard.Sleep(1000).ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_C).Sleep(50);
+            string str = ClipboardGetText();
+            Process.Start(new ProcessStartInfo { FileName = $"https://translate.google.cn/?hl=zh-CN&sl=zh-CN&tl=en&text={str}&op=translate", UseShellExecute = true });
+        }
+
+        private string ClipboardGetText()
+        {
+            Thread.Sleep(50);
+            string str = "";
+            if (Clipboard.ContainsText())
+            {
+                try { str = Clipboard.GetText(); }
+                catch (COMException) { }
+                finally
+                {
+                    Thread.Sleep(20);
+                    try { str = Clipboard.GetText(); }
+                    catch (COMException) { str = "剪切板被占用"; }
+                }
+            }
+            return str.Trim().Replace("  ", "");
+        }
+    }
+}
